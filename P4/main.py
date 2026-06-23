e98fb88610c4b3c55b3908f31297424e6e490d58
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-23 20:19:49 +0800

    feat: add highest, sort descending, and average methods

--- P4/main.py
+++ P4/main.py
@@ -26,3 +26,32 @@ class NumberProcessor:
     def most_frequent(self):
         numbers = self.read_numbers()
         return max(set(numbers), key=numbers.count)
+class NumberProcessor:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_numbers(self):
+        with open(self.filename, 'r') as file:
+            numbers = [int(num) for num in file.read().split()]
+        return numbers
+
+    def find_duplicates(self):
+        numbers = self.read_numbers()
+        duplicates = set([n for n in numbers if numbers.count(n) > 1])
+        return duplicates
+
+    def most_frequent(self):
+        numbers = self.read_numbers()
+        return max(set(numbers), key=numbers.count)
+
+    def highest_number(self):
+        numbers = self.read_numbers()
+        return max(numbers)
+
+    def sort_descending(self):
+        numbers = self.read_numbers()
+        return sorted(numbers, reverse=True)
+
+    def average(self):
+        numbers = self.read_numbers()
+        return sum(numbers) / len(numbers)
