43ca07f3199b514be00d16511a1d646f4d8889bd
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-23 19:56:35 +0800

    feat: implement read_numbers method

--- P1/main.py
+++ P1/main.py
@@ -1,3 +1,11 @@
 class NumberSeparator:
     def __init__(self, source_file):
         self.source_file = source_file
+class NumberSeparator:
+    def __init__(self, source_file):
+        self.source_file = source_file
+
+    def read_numbers(self):
+        with open(self.source_file, 'r') as file:
+            numbers = [int(num) for num in file.read().split()]
+        return numbers
