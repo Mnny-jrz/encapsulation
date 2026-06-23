ca781497ae0c896f5f5ddb258f18d0986eb1b686
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-24 02:26:23 +0800

    polish

--- encapsulation/PET/pet.py
+++ encapsulation/PET/pet.py
@@ -3,14 +3,26 @@ class Pet:
         self.__name = ""
         self.__animal_type = ""
         self.__age = 0
+
+    # Setters
     def set_name(self, name): self.__name = name
+    def set_animal_type(self, animal_type): self.__animal_type = animal_type
+    def set_age(self, age): self.__age = age
+
+    # Getters
     def get_name(self): return self.__name
-    # same for type and age
+    def get_animal_type(self): return self.__animal_type
+    def get_age(self): return self.__age
+
+
 def main():
     pet = Pet()
     pet.set_name("Buddy")
     pet.set_animal_type("Dog")
     pet.set_age(3)
     print(pet.get_name(), pet.get_animal_type(), pet.get_age())
+
+
 if __name__ == "__main__":
     main()
+
