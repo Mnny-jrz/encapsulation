93b763e8a2608b2f1a7d8f09f550f2d603856d5f
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-24 02:19:55 +0800

    add test program for Pet

--- encapsulation/PET/pet.py
+++ encapsulation/PET/pet.py
@@ -6,3 +6,9 @@ class Pet:
     def set_name(self, name): self.__name = name
     def get_name(self): return self.__name
     # same for type and age
+def main():
+    pet = Pet()
+    pet.set_name("Buddy")
+    pet.set_animal_type("Dog")
+    pet.set_age(3)
+    print(pet.get_name(), pet.get_animal_type(), pet.get_age())
