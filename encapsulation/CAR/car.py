e959d7c63d6c93ab974e205d577ac0cd58243ecc
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-24 02:21:28 +0800

    add test program for Car

--- encapsulation/CAR/car.py
+++ encapsulation/CAR/car.py
@@ -5,3 +5,7 @@ class Car:
         self.__speed = 0
     def accelerate(self): self.__speed += 5
     def brake(self): self.__speed -= 5
+def main():
+    car = Car(2024, "Toyota")
+    for _ in range(5): car.accelerate(); print(car.get_speed())
+    for _ in range(5): car.brake(); print(car.get_speed())
