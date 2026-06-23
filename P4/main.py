44414d2cd004ad0508ed46f72aa2ba295590d39d
Mnny-jrz <manny.taoatao.1@gmail.com>
2026-06-23 20:19:23 +0800

    feat: add methods for duplicates and most frequent number

--- P4/main.py
+++ P4/main.py
@@ -9,3 +9,20 @@ class NumberProcessor:
         with open(self.filename, 'r') as file:
             numbers = [int(num) for num in file.read().split()]
         return numbers
+class NumberProcessor:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_numbers(self):
+        with open(self.filename, 'r') as file:
+            numbers = [int(num) for num in file.read().split()]
+        return numbers
+
+    def find_duplicates(self):
+        numbers = self.read_numbers()
+        duplicates = set([n for n in numbers if numbers.count(n) > 1])
+        return duplicates
+
+    def most_frequent(self):
+        numbers = self.read_numbers()
+        return max(set(numbers), key=numbers.count)
